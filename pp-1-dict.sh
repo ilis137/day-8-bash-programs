5bff6a4231d344bdd13094d26b0137948d67a592
ilis137 <iliyasshahapure1@gmail.com>
2022-09-01 09:37:58 +0530

    add practice problem files

---
+++ pp-1-dict.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+declare -A dictionary=(
+                            [1]=0
+                            [2]=0
+                            [3]=0
+                            [4]=0
+                            [5]=0
+                            [6]=0
+                        )
+maxCount=10
+while(( 1 ))
+do
+    result=$(( RANDOM % 6 + 1 ))
+    (( dictionary[$result]++))
+    if(( dictionary[$result] == maxCount))
+    then
+        break
+    fi
+
+done
+mostFrequentNumber=$(( RANDOM % 6 + 1 ))
+leastFrequentNumber=$(( RANDOM % 6 + 1 ))
+
+echo "The numbers of the dice and their corresponding frequencies are:"
+
+for number in ${!dictionary[@]}
+do
+    if(( dictionary[$number] >= dictionary[$mostFrequentNumber] ))
+    then
+        most_frequent_number=$number
+    fi
+    if(( dictionary[$number] <= dictionary[$leastFrequentNumber] ))
+    then
+        least_frequent_number=$number
+    fi
+
+    echo $number " " ${dictionary[$number]}
+done
+
+echo "The number that reached maximum times is $mostFrequentNumber"
+echo "The number that reached minimum times is $leastFrequentNumber"
